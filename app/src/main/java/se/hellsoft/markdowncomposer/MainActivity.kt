dfdd498076e63d33678fea7ec4cd7bbaedafff87
ziem <zprzemek@vp.pl>
2022-01-08 11:21:57 +0100

    Fix MainActivity

--- app/src/main/java/se/hellsoft/markdowncomposer/MainActivity.kt
+++ app/src/main/java/se/hellsoft/markdowncomposer/MainActivity.kt
@@ -1,22 +1,26 @@
 package se.hellsoft.markdowncomposer
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Box
-import androidx.compose.foundation.ScrollableColumn
-import androidx.compose.foundation.Text
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.gesture.doubleTapGestureFilter
-import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.text.annotatedString
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.ui.tooling.preview.Preview
 import org.commonmark.node.Document
 import org.commonmark.parser.Parser
 import se.hellsoft.markdowncomposer.ui.MarkdownComposerTheme
@@ -27,15 +31,19 @@ class MainActivity : AppCompatActivity() {
         val parser = Parser.builder().build()
         val root = parser.parse(MIXED_MD) as Document
         setContent {
-            val (render, setRender) = remember { mutableStateOf(true) }
+            var render by remember { mutableStateOf(true) }
             MarkdownComposerTheme {
                 Surface(
                     color = MaterialTheme.colors.background,
-                    modifier = Modifier.doubleTapGestureFilter {
-                        setRender(!render)
+                    modifier = Modifier.pointerInput(Unit) {
+                        detectTapGestures(
+                            onDoubleTap = {
+                                render = !render
+                            }
+                        )
                     }) {
-                    Box(paddingStart = 16.dp, paddingEnd = 16.dp) {
-                        ScrollableColumn {
+                    Box(modifier = Modifier.padding(start = 16.dp, end = 16.dp)) {
+                        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
                             if (render) {
                                 MDDocument(root)
                             } else {
@@ -43,7 +51,6 @@ class MainActivity : AppCompatActivity() {
                             }
                         }
                     }
-
                 }
             }
         }
@@ -57,7 +64,8 @@ fun DefaultPreview() {
     val root = parser.parse(MIXED_MD) as Document
     MarkdownComposerTheme {
         Surface(
-            color = MaterialTheme.colors.background) {
+            color = MaterialTheme.colors.background
+        ) {
             Box {
                 MDDocument(root)
             }
